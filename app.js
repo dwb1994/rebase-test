b92c57f1dd8f3d3b0f2fbe4adcf8e8ca9f00e1b3
dongwenbo <dongwenbo@kuaishou.com>
2021-08-18 17:40:37 +0800

    dev 1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
  * @file: description
  * @author: dongwenbo
  * @LastEditors: dongwenbo
- * @LastEditTime: 2021-08-18 17:10:21
+ * @LastEditTime: 2021-08-18 17:40:23
  */
 console.log(1);
+// dev 1
